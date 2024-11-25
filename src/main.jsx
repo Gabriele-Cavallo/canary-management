54e4889d5c2190127887ad177af6b900cb369fa1
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-25 16:09:08 +0100

    feat: add RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import './index.scss'
 import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import CanaryList from './pages/CanaryList.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/canary-list',
+        element: <CanaryList />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
